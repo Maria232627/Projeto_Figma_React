6edb8e02c58bfeeacb52412ede331d56b921f837
Maria232627 <maria.lscarvalho01@gmail.com>
2023-08-18 08:44:16 -0300

    Segunda atualização?

---
+++ src/services/dados.ts
@@ -0,0 +1,26 @@
+export const dados = [
+    {
+      id: 1,
+      data: "17/08/2023 18:00:00",
+      titulo: "Caquinha",
+      mensagem: "Rua dos Planetas, n°71"
+    },
+    {
+      id: 2,
+      data: "23/06/2023 07:10:00",
+      titulo: "Milkshake",
+      mensagem: "Rua Fim do Mundo, n°1000"
+    },
+    {
+      id: 3,
+      data: "23/06/2023 07:20:00",
+      titulo: "Chá",
+      mensagem: "Rua do Seu José, n°35"
+    },
+    {
+      id: 4,
+      data: "23/06/2023 07:30:00",
+      titulo: "Caquinha",
+      mensagem: "Rua dos Bobos, n°0"
+    },
+  ]
